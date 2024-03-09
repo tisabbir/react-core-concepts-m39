0065be4693843de87d0ebac68a57bcff7253b236
TI Sabbir <tisabbir0@gmail.com>
2024-03-09 12:34:57 +0600

    2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    alert("btn1 clicked");
+  };
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Vite + React</h3>
+      <button onClick={handleClick}>Btn 1</button>
+
+      <button
+        onClick={() => {
+          alert("btn 2 clicked");
+        }}
+      >
+        Btn 2
+      </button>
+
+      <button
+        onClick={() => {
+          addToFive(3);
+        }}
+      >
+        Add to 5
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
